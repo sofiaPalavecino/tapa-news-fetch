3fdec9197993560fb8472a9e855c3fdf7c2f96ee
sofiaPalavecino <sofiaor29@gmail.com>
2024-05-02 21:26:39 -0300

    Created el diario standarizer

---
+++ standarizers/elDiarioARStandarizer.js
@@ -0,0 +1,34 @@
+const Standarizer = require("../standarizer");
+
+class ElDiarioARStandarizer extends Standarizer{
+    constructor(){
+        const pageName = "eldiarioar"
+        super(pageName)
+    }
+
+    preProcessItems(pageItems){
+        pageItems = pageItems.filter((item) => this.filterByTopic(item))
+        return pageItems
+    }
+
+    filterByTopic(item){
+        let validTopics = ["politica", "economia", "sociedad", "blog", "opinion", "latinoamerica"]
+        let link = item[this.structure['link']][0];
+        let topic = link.replace('https://www.eldiarioar.com/','').split('/')[0]
+        return validTopics.includes(topic)
+    }
+
+    getImage(item, newItem){
+        if (item.hasOwnProperty(this.structure['image'])) {
+            let image = item[this.structure['image']][0]['$']['url'];
+            newItem['image'] = image;
+        }
+    }
+
+    processCustomStandarization(item, newItem){
+        this.getImage(item, newItem);
+    }
+
+}
+
+module.exports = ElDiarioARStandarizer;
