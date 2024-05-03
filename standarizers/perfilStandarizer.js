320370d9ebf489a21bfa60fbcecbb2d56b76ed9f
sofiaPalavecino <sofiaor29@gmail.com>
2024-05-02 21:27:44 -0300

    Created perfil standarizer

---
+++ standarizers/perfilStandarizer.js
@@ -0,0 +1,30 @@
+const Standarizer = require("../standarizer");
+
+class PerfilStandarizer extends Standarizer{
+    constructor(){
+        const pageName = "perfil"
+        super(pageName)
+    }
+
+    getSubtitle(item, newItem){
+        if (item.hasOwnProperty(this.structure['subtitle'])){
+            let subtitle = item[this.structure['subtitle']][0];
+            subtitle = subtitle.replace(/<[^>]*>/g, '').replace(/Leer más/g, '');
+            let sentences = this.getFirstSentenceArray(subtitle)
+            newItem['subtitle'] = sentences == null ? subtitle.trim() : sentences[0].trim()
+        }
+    }
+    getImage(item, newItem){
+        if (item.hasOwnProperty(this.structure['image'])) {
+            let image = item[this.structure['image']][0]['$']['url'];
+            newItem['image'] = image;
+        }
+    }
+
+    processCustomStandarization(item, newItem){
+        this.getSubtitle(item, newItem);
+        this.getImage(item, newItem);
+    }
+}
+
+module.exports = PerfilStandarizer;
